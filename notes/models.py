aadd643b7320eb6df9f206fcd9b4ec3f09a9244c
nikita-solovyov-dev <156105775+nikita-solovyov-dev@users.noreply.github.com>
2024-02-25 21:49:37 +0300

    Created TextNote model and its migration

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class TextNote(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return self.text if len(self.text) < 60 else f"{self.text[:60]}..."
