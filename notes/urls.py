84f8eee35145afbc621d16c613f0bc1720c20fea
nikita-solovyov-dev <156105775+nikita-solovyov-dev@users.noreply.github.com>
2024-02-25 22:21:48 +0300

    Added basic templates and NotesTemplateView

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 
+from notes.views import NotesTemplateView
+
 app_name = "notes"
 
 urlpatterns = [
     path("api/", include("notes.api.urls", namespace="api")),
+    path("", NotesTemplateView.as_view(), name="notes"),
 ]
