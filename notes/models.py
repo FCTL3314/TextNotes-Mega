df93de1ba0e107d34e9ba1f11eb7b4d36db2c5a7
XXXDESHXXX <kirya.shvarts@list.ru>
2024-02-26 22:30:43 +0300

    Added mega path field into TextNote model

--- notes/models.py
+++ notes/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 class TextNote(models.Model):
     title = models.CharField(max_length=128)
     text = models.TextField()
+    mega_path = models.FilePathField()
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now_add=timezone.now)
 
