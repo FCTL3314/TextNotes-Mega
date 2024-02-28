8792e2163558b177e8e4d0235f7c2d1d5bb53c76
nikita-solovyov-dev <156105775+nikita-solovyov-dev@users.noreply.github.com>
2024-02-28 23:30:19 +0300

    Change styles; Move js, css code to the separated modules; Add settings for media and static files

--- core/settings.py
+++ core/settings.py
@@ -121,6 +121,16 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+if DEBUG:
+    STATICFILES_DIRS = (BASE_DIR / "static",)
+else:
+    STATIC_ROOT = BASE_DIR / "static"
+
+# Media files
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
---
+++ static/js/loading.js
@@ -0,0 +1,9 @@
+const loadingWrapper = $("#loadingWrapper");
+
+function showLoading() {
+    loadingWrapper.show();
+}
+
+function hideLoading() {
+    loadingWrapper.hide();
+}
