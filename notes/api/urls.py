2232b69b43188970086226b600eb1e00f4aa318c
XXXDESHXXX <kirya.shvarts@list.ru>
2024-03-06 10:16:23 +0300

    Create DeleteAPIView for notes

--- notes/api/urls.py
+++ notes/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from notes.api.views import TextNoteCreateAPIView
+from notes.api.views import TextNoteCreateAPIView, TextNoteDeleteAPIView
 
 app_name = "notes"
 
 urlpatterns = [
     path("textnotes/create/", TextNoteCreateAPIView.as_view(), name="textnote_create"),
+    path("textnotes/delete/", TextNoteDeleteAPIView.as_view(), name="textnote_delete"),
 ]
--- notes/api/views.py
+++ notes/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework import status
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, DestroyAPIView
 from rest_framework.response import Response
 
-from notes.api.serializers import TextNoteSerializer
+from notes.models import TextNote
 
 
 class TextNoteCreateAPIView(CreateAPIView):
@@ -14,3 +14,17 @@ class TextNoteCreateAPIView(CreateAPIView):
         return Response(
             serializer.data, status=status.HTTP_201_CREATED, headers=headers
         )
+
+
+class TextNoteDeleteAPIView(DestroyAPIView):
+    def delete(self, request, *args, **kwargs):
+        ids = request.GET.getlist("ids")
+        deleted_notes = []
+        for note_id in ids:
+            try:
+                note = TextNote.objects.get(id=note_id)
+                note.delete()
+                deleted_notes.append(note_id)
+            except TextNote.DoesNotExist:
+                pass
+        return Response({"deleted_notes": deleted_notes})
